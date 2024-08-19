daad17b36440e40d2183c68dd950333792c2cf91
menshikovnik <nikmenshikov1@gmail.com>
2024-08-19 03:43:30 +0300

    add tests

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -29,11 +29,23 @@ dependencies {
 
     runtimeOnly("org.postgresql:postgresql")
 
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.junit.jupiter:junit-jupiter-api")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+
+    testImplementation("org.mockito:mockito-core:4.4.0")
+
     compileOnly("org.projectlombok:lombok:1.18.20")
     annotationProcessor("org.projectlombok:lombok:1.18.20")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+    useJUnit()
+
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
 
 java {
--- backend/src/main/java/com/testmanagementsystem/entity/TestResult.java
+++ backend/src/main/java/com/testmanagementsystem/entity/TestResult.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
-import java.util.List;
 
 @Data
 @Entity
@@ -17,8 +16,6 @@ public class TestResult {
     private Test test;
     @ManyToOne
     private InviteToken inviteToken;
-    @OneToMany(cascade = CascadeType.ALL)
-    private List<Answer> answers;
     private String name;
     private String surname;
     @Email
